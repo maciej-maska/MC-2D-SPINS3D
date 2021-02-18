3fc20cb5cd48030cb198ad26d6bc4dd5f5d31203
maciej-maska <maciej@maska.eu.org>
2021-02-18 21:54:17 +0100

    obliczanie koncentracji elektronów

--- mc_3d.f90
+++ mc_3d.f90
@@ -26,14 +26,14 @@ program majorana
        complex(8),intent(in) :: h(:,:)
        complex(8),intent(out) :: g(:,:)
        real(8) :: omega
-     end subroutine green
+     end subroutine green 
      function topo(ang_xy_tab,ang_z_tab)
        real(8) :: topo, ang_xy_tab(:), ang_z_tab(:)
      end function topo
   end interface
   real(8), allocatable :: e(:), ang_xy(:), ang_z(:), rnd(:)
-  real(8) :: free_energy, r, free_en, old_free_en, en, old_en, c_z, ang_xy_old, ang_z_old
-  complex(8), allocatable :: g(:,:), h(:,:), h1(:,:)
+  real(8) :: free_energy, r, free_en, old_free_en, en, old_en, c_z, ang_xy_old, ang_z_old, cosine, cosine_min = -1._8, an, an_up, an_dn
+  complex(8), allocatable :: g(:,:), h(:,:), h1(:,:), u_up(:,:), u_dn(:,:), v_up(:,:), v_dn(:,:)
   complex(8) :: h_old(8)
   integer :: i, j, info, in, k, l, i_therm
   logical :: old_config
@@ -46,7 +46,7 @@ program majorana
 
   n = lx*ly
 
-  allocate(e(4*n), g(4*n,4*n), h(4*n,4*n), h1(4*n,4*n), ang_xy(n), ang_z(n), rnd(n))
+  allocate(e(4*n), g(4*n,4*n), h(4*n,4*n), h1(4*n,4*n), ang_xy(n), ang_z(n), rnd(n), u_up(n,4*n), u_dn(n,4*n), v_up(n,4*n), v_dn(n,4*n))
   i_therm = 0
   inquire(FILE="spins_3d.dat", EXIST=old_config)
   if (old_config) then
@@ -56,29 +56,39 @@ program majorana
      enddo
   else
      call random_number(rnd)
-     ang_xy=2*pi*rnd
+     ang_xy=2*pi*rnd                          ! phi
      call random_number(rnd)
-     ang_z=acos(2*rnd - 1)
+     ang_z=acos(2*rnd - 1)                    ! theta
      !ang_z=0.5*pi
   endif
-
   call hamiltonian(h,ang_xy,ang_z)
   h1=h
-  call heevd(h1, e, info=info)
+  call heevd(h1, e, jobz='V', info=info)
+  u_up=h1(1:n,:)
+  u_dn=h1(n+1:2*n,:)
+  v_dn=h1(2*n+1:3*n,:)
+  v_up=h1(3*n+1:4*n,:)
+  an_up = sum(abs(u_up(:,:2*n))**2)/n  ! brakuje funkcji F-D (T=0)
+  an_dn = sum(abs(v_dn(:,2*n+1:))**2)/n  ! brakuje funkcji F-D (T=0)
+
   i = 0        
   old_free_en = free_energy(e)
   old_en = sum(e(1:2*n))
-  print *,old_free_en   !,topo(ang_xy,ang_z)
-
+  print *,old_free_en, an_up+an_dn   !,topo(ang_xy,ang_z)
+  
   main_loop: do !###############################################################
      call random_number(r)
      in = int(n*r) + 1
      ang_xy_old = ang_xy(in)
      ang_z_old = ang_z(in)
-     call random_number(r)
-     ang_xy(in) = 2*pi*r  
-     call random_number(r)
-     ang_z(in) = acos(2*r-1)
+!     do
+        call random_number(r)
+        ang_xy(in) = 2*pi*r  
+        call random_number(r)
+        ang_z(in) = acos(2*r-1)
+!        cosine = sin(ang_z(in))*sin(ang_z_old)*cos(ang_xy(in)-ang_xy_old) + cos(ang_z(in))*cos(ang_z_old) ! cosine between the new and old angles
+!        if (cosine < cosine_min) exit
+!     enddo
      !ang_z(in)=0.5*pi
      h_old(1) = h(in,in+n)
      h_old(2) = h(in+n,in)
@@ -113,7 +123,7 @@ program majorana
 
         h(in,in+n) = h_old(1)
         h(in+n,in) = h_old(2)
-        h(in+2*n,in+3*n) = h_old(3)
+        h(in+2*n,in+3*n) = h_old(3) 
         h(in+3*n,in+2*n) = h_old(4)
         h(in,in) = h_old(5)
         h(in+n,in+n) = h_old(6)
@@ -130,7 +140,15 @@ program majorana
            write(10,*) ang_xy(l), ang_z(l)
         enddo
         close(10)
-        print *,free_en  !,topo(ang_xy,ang_z)
+        h1=h
+        call heevd(h1, e, jobz='V', info=info)
+        u_up=h1(1:n,:)
+        u_dn=h1(n+1:2*n,:)
+        v_dn=h1(2*n+1:3*n,:)
+        v_up=h1(3*n+1:4*n,:)
+        an_up = sum(abs(u_up(:,:2*n))**2)/n  ! brakuje funkcji F-D (T=0)
+        an_dn = sum(abs(v_dn(:,2*n+1:))**2)/n  ! brakuje funkcji F-D (T=0)
+        print *,free_en, an_up+an_dn  !,topo(ang_xy,ang_z)
      endif
   enddo main_loop !#############################################################
 end program majorana
@@ -244,170 +262,3 @@ free_energy=-temperature*free_energy
 end function free_energy
 !
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-!
-function topo(ang_xy_tab, ang_z_tab)
-  use stale
-  use lapack95
-  implicit none
-
-  interface
-     function ainv(a)
-       complex(8),dimension(4,4) :: ainv,a
-     end function ainv
-     function cdot(w2,w1)
-       complex(8),dimension(8,8) :: cdot,w2,w1
-     end function cdot
-  end interface
-
-  complex(8) :: tt(4,4),hh(4,4),uu(8,8),mbar(8,8),ww(8,8),ww1(8,8),mm(8,8),det
-  real(8) :: ang_xy, ang_z, topo, s_xy, c_xy, s_z, c_z
-  real(8) :: ang_xy_tab(:), ang_z_tab(:)
-  integer :: i
-
-  uu = 0
-  do i=1,4
-     uu(i,i) = cmplx(1,0)
-     uu(i,i+4) = cmplx(1,0)
-     uu(i+4,i) = cmplx(0,1)
-     uu(i+4,i+4) = cmplx(0,-1)
-  enddo
-  uu = uu/sqrt(2._8)
-
-  do i=1,n-1
-     ang_xy = ang_xy_tab(i)
-     ang_z = ang_z_tab(i)
-     s_xy = sin(ang_xy_tab(i))
-     c_xy = cos(ang_xy_tab(i))
-     s_z = sin(ang_z_tab(i))
-     c_z = cos(ang_z_tab(i))
-     tt = cmplx(0._8,0._8)
-!     if (i < n) then
-        tt(1,1) = cmplx(t, alphay)
-        tt(2,2) = cmplx(t, -alphay)
-        tt(3,3) = cmplx(-t, alphay)
-        tt(4,4) = cmplx(-t, -alphay)
-        tt(1,2) = cmplx(alphaz, 0)
-        tt(2,1) = cmplx(-alphaz, 0)
-        tt(3,4) = cmplx(-alphaz, 0)
-        tt(4,3) = cmplx(alphaz, 0)
-!     endif
-!     if (i==n/2) then
-!        tt(1,1) = 0.01*cmplx(t, alphay)
-!        tt(2,2) = 0.01*cmplx(t, -alphay)
-!        tt(3,3) = 0.01*cmplx(-t, alphay)
-!        tt(4,4) = 0.01*cmplx(-t, -alphay)
-!        tt(1,2) = 0.01*cmplx(alphaz, 0)
-!        tt(2,1) = 0.01*cmplx(-alphaz, 0)
-!        tt(3,4) = 0.01*cmplx(-alphaz, 0)
-!        tt(4,3) = 0.01*cmplx(alphaz, 0)           
-!     endif  
-
-     hh = cmplx(0._8,0._8)
-     hh(1,1) = Bz - cp + 0.5d0*J_spin*c_z
-     hh(2,2) = -Bz - cp - 0.5d0*J_spin*c_z
-     hh(3,3) = Bz + cp + 0.5d0*J_spin*c_z
-     hh(4,4) = -Bz + cp - 0.5d0*J_spin*c_z
-     hh(1,3) = delta
-     hh(3,1) = delta
-     hh(2,4) = delta
-     hh(4,2) = delta
-     hh(1,2) = 0.5d0*J_spin*s_z*cmplx(c_xy,-s_xy)
-     hh(2,1) = 0.5d0*J_spin*s_z*cmplx(c_xy,s_xy)
-     hh(3,4) = hh(1,2)
-     hh(4,3) = hh(2,1)
-
-     mbar(1:4,1:4) = 0
-     mbar(1:4,5:8) = transpose(conjg(tt))
-     mbar(5:8,1:4) = -ainv(tt)
-     mbar(5:8,5:8) = -matmul(ainv(tt),hh)
-
-     mm = matmul(matmul(transpose(conjg(uu)),mbar),uu)
-     
-     ww1(1:4,1:4) = -matmul(ainv(mm(5:8,5:8)),mm(5:8,1:4))
-     ww1(1:4,5:8) = ainv(mm(5:8,5:8))
-     ww1(5:8,1:4) = mm(1:4,1:4) - matmul(matmul(mm(1:4,5:8),ainv(mm(5:8,5:8))),mm(5:8,1:4))
-     ww1(5:8,5:8) = matmul(mm(1:4,5:8),ainv(mm(5:8,5:8)))
-
-     if (i==1) then
-        ww = ww1
-     else
-        ww = cdot(ww1,ww)
-     endif
-  enddo
-  topo = real(det(ww(1:4,1:4)))
-end function topo
-!
-!####################################################################################################
-!
-function cdot(w2,w1)
-  implicit none
-  interface
-     function ainv(a)
-       complex(8),dimension(4,4) :: ainv,a
-     end function ainv
-  end interface
-  complex(8),dimension(8,8) :: cdot,w2,w1
-  complex(8),dimension(4,4) :: u2,v2,r2,s2,u1,v1,r1,s1,ai,tmp1,tmp2
-  integer :: i
-  u2 = w2(1:4,1:4)
-  v2 = w2(1:4,5:8)
-  r2 = w2(5:8,1:4)
-  s2 = w2(5:8,5:8)
-  u1 = w1(1:4,1:4)
-  v1 = w1(1:4,5:8)
-  r1 = w1(5:8,1:4)
-  s1 = w1(5:8,5:8)
-  ai = 0
-  do i=1,4; ai(i,i) = 1; enddo
-     tmp1 = ainv(ai - matmul(u2,s1))
-     cdot(1:4,1:4) = u1 + matmul(matmul(matmul(v1,tmp1),u2),r1)
-     cdot(1:4,5:8) = matmul(matmul(v1,tmp1),v2)
-     tmp2 = ainv(ai - matmul(s1,u2))
-     cdot(5:8,1:4) = matmul(matmul(r2,tmp2),r1)
-     cdot(5:8,5:8) = s2 + matmul(matmul(matmul(r2,tmp2),s1),v2)
-end function cdot
-
-function ainv(a)
-  implicit none
-  complex(8),dimension(4,4) :: ainv,a
-  complex(8) :: work(4)
-  integer(8),dimension(4) :: ipiv
-  integer :: info
-  ainv = a
-  call zgetrf(4,4,ainv,4,ipiv,info)
-  if (info /= 0) then
-     print *,'GETRF',info
-     stop 'GETRF'
-  endif
-  call zgetri(4,ainv,4,ipiv,work,4,info)
-  if (info /= 0) then
-     print *,'GETRI',info
-     stop 'GETRI'
-  endif
-end function ainv
-
-function det(mat)
-  implicit none
-  integer(kind=8),parameter :: N=4_8
-  complex(kind=8) :: det
-  complex(kind=8), intent(inout), dimension(N,N) :: mat
-  integer(kind=8) :: i, info
-  integer :: ipiv(N)
-  real(kind=8) :: sgn 
-  ipiv = 0
-  call zgetrf(N, N, mat, N, ipiv, info)
-  det = dcmplx(1,0)
-  do i = 1, N
-     det = det*mat(i, i)
-  end do
-  sgn = 1_8
-  do i = 1, N
-     if(ipiv(i) /= i) then
-        sgn = -sgn
-     end if
-  end do
-  det = sgn*det   
-end function det
-!
-!#####################################################################################
-!
